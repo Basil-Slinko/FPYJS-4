83f3741131cea31e51480a792074a46a14f5e310
Basil-Slinko <basil.slinko@gmail.com>
2022-06-09 11:26:13 +0300

    Task One

---
+++ 2. Операторы цикла, функции и объекты/index.js
@@ -0,0 +1,53 @@
+// function fibo(n) {
+//     if (n <= 1) {
+//         return n
+//     };     
+//     return fibo(n-2) + fibo(n-1);
+// }
+
+
+// console.log(fibo(process.argv[2]));
+
+// // console.log(fibo(10));
+
+function sum(n) {
+    let a = n + 100
+    return a
+}
+
+console.log(sum(process.argv[2]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function fibo(n) {
+//     if (n <= 1) {
+//         return n
+//     };     
+//     return fibo(n-2) + fibo(n-1);
+// }
+
+
+// console.log(fibo(99));
+
+// // console.log('Hellow World!');
